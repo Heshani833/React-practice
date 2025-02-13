b075910aebad31aa49c6539c25d2c09116f4c65d
Heshani833 <heshanishehana123@gmail.com>
2025-02-13 15:31:17 +0530

    Conditional rendering - Switch Case Statements

--- src/components/ListGroupNew.tsx
+++ src/components/ListGroupNew.tsx
@@ -3,25 +3,22 @@ import React from "react";
 const ListGroupNew = () => {
   const items: string[] = ["Colombo", "Kandy", "Badulla", "Matara", "Galle"];
 
-  let message;
-  if (items.length === 0) {
-    message = <p>Items are not found</p>;
-  } else {
-    message = <p>Items are found</p>;
+  switch (items.length) {
+    case 0:
+      return <p>No items available.</p>;
+    case 1:
+      return <p>One item available.</p>;
+    case 2:
+      return <p>Two items available.</p>;
+    case 3:
+      return <p>Three items available.</p>;
+    case 4:
+      return <p>Four items available.</p>;
+    case 5:
+      return <p>Five items available.</p>;
+    default:
+      return <p>More than five items available.</p>;
   }
-
-  return (
-    <>
-      <ul className="list-group">
-        {items.map((item, index) => (
-          <li key={index} className="list-group-item">
-            {item}
-          </li>
-        ))}
-        {message}
-      </ul>
-    </>
-  );
 };
 
 export default ListGroupNew;
