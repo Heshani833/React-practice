9dd9bb95ee69610d3e73152abd43c9b74292b6af
Heshani833 <heshanishehana123@gmail.com>
2025-02-13 15:06:03 +0530

    Conditional rendering-Ternary Operators

--- src/components/ListGroupNew.tsx
+++ src/components/ListGroupNew.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 
 const ListGroupNew = () => {
-  const items: string[] = [];
-  return <>{items.length === 0 && <p>Items are not found</p>}</>;
+  const items: string[] = ["Colombo", "Kandy", "Badulla", "Matara", "Galle"];
+  return (
+    <>
+      <ul className="list-group">
+        {items.length === 0 ? (
+          <p>Items are not found.</p>
+        ) : (
+          <p>Items are found</p>
+        )}
+      </ul>
+    </>
+  );
 };
 
 export default ListGroupNew;
