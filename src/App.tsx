e822045ce1f00cc7ed8693521f1a06eff9a4f82f
Heshani833 <heshanishehana123@gmail.com>
2025-02-27 23:42:24 +0530

    Adding New Array Elements

--- src/App.tsx
+++ src/App.tsx
@@ -2,41 +2,18 @@ import { useState } from "react";
 import ListGroup from "./components/ListGroup/ListGroup";
 
 function App() {
-  const [items, setItems] = useState(["colombo", "Kandy", "Galle"]);
-  const [foods, setFoods] = useState(["Pizza", "Rice", "Ice Cream"]);
-
-  const [person, setPerson] = useState({
-    name: {
-      firstName: "Heshani",
-      lastName: "Shehana",
-    },
-    addres: {
-      country: "Sri Lanka",
-      street: {
-        streetName: "Colombo Road",
-        number: 2,
-      },
-    },
-  });
-
-  const [isLoading, setIsLoading] = useState(false);
-  const handleChangeName = () => {
-    setPerson({
-      ...person,
-      addres: {
-        ...person.addres,
-        street: {
-          ...person.addres.street,
-          number: 19,
-        },
-      },
-    });
+  const [tags, setTags] = useState(["tag1", "tag2", "tag3"]);
+  const handleChangeTags = () => {
+    setTags([...tags, "tag4"]);
   };
 
   return (
     <>
-      <h1>{person.addres.street.number}</h1>
-      <button onClick={handleChangeName}>Click</button>
+      {tags.map((tag, index) => (
+        <h1 key={index}>{tag}</h1>
+      ))}
+
+      <button onClick={handleChangeTags}>Click Here</button>
     </>
   );
 }
